ec577511529b1bec6f2a9a324d52de666fce37b6
JORGE ANDRES GOMEZ VILLAMIZAR <ja.gomez1@sis.virtual.uniandes.edu.co>
2018-03-19 19:30:33 -0500

    Identation error

--- mensajeria.py
+++ mensajeria.py
@@ -7,9 +7,10 @@ def index():
     return "Hello, World!"
 
 @app.route('/postTest', methods=['POST'])
-def testMethod():	content = request.json
-	x = 'el correo se ha enviado existosamente a ' + content['correoReceptor'] + ' debido a que ' + content['cuerpo'] + '.'
-	return jsonify({'msg': x})
+def testMethod():
+    content = request.json
+    x = 'el correo se ha enviado existosamente a ' + content['correoReceptor'] + ' debido a que ' + content['cuerpo'] + '.'
+    return jsonify({'msg': x})
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0',port=8080)
