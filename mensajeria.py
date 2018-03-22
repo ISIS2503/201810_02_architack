4457a137bf66b56254b8eb35ab254543207b7874
JORGE ANDRES GOMEZ VILLAMIZAR <ja.gomez1@sis.virtual.uniandes.edu.co>
2018-03-22 15:57:25 -0500

    Mensajeria

--- mensajeria.py
+++ mensajeria.py
@@ -1,16 +1,35 @@
 from flask import Flask, jsonify, request
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
 app = Flask(__name__)
 
+def sendMail(receptor, asunto, cuerpo):
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login("jagv1998@gmail.com", "bvelmbnnqeytvyki")
+
+	msg = MIMEMultipart()
+	msg['Subject'] = asunto
+
+	body = cuerpo
+
+	msg.attach(MIMEText(body, 'plain'))
+	server.sendmail("jagv1998@gmail.com", receptor, msg.as_string())
+	print("Mail sent.")
+	server.close()
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+	return "Bienvenido al sistema de mensajeria"
 
-@app.route('/postTest', methods=['POST'])
+@app.route('/alarm', methods=['POST'])
 def testMethod():
-    content = request.json
-    x = 'el correo se ha enviado existosamente a ' + content['correoReceptor'] + ' debido a que ' + content['cuerpo'] + '.'
-    return jsonify({'msg': x})
+	content = request.json
+	x = 'el correo se ha enviado existosamente a ' + content['correoReceptor'] + ' debido a que ' + content['cuerpo'] + '.'
+	#sendMail(content['correoReceptor'],content['asunto'],content['cuerpo'])
+	return jsonify({'msg': x})
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0',port=8080)
+	app.run(debug=False,host='0.0.0.0',port=8080)
