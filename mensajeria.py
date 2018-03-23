8256b73c0a4986dd123a4c85276bffe43796b74e
JORGE ANDRES GOMEZ VILLAMIZAR <ja.gomez1@sis.virtual.uniandes.edu.co>
2018-03-23 08:53:12 -0500

    Servicio de mensajeria ligero

--- mensajeria.py
+++ mensajeria.py
@@ -5,31 +5,13 @@ import smtplib
 
 app = Flask(__name__)
 
-def sendMail(receptor, asunto, cuerpo):
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login("jagv1998@gmail.com", "bvelmbnnqeytvyki")
-
-	msg = MIMEMultipart()
-	msg['Subject'] = asunto
-
-	body = cuerpo
-
-	msg.attach(MIMEText(body, 'plain'))
-	server.sendmail("jagv1998@gmail.com", receptor, msg.as_string())
-	print("Mail sent.")
-	server.close()
-
 @app.route('/')
 def index():
 	return "Bienvenido al sistema de mensajeria"
 
 @app.route('/alarm', methods=['POST'])
 def testMethod():
-	content = request.json
-	x = 'el correo se ha enviado existosamente a ' + content['correoReceptor'] + ' debido a que ' + content['cuerpo'] + '.'
-	#sendMail(content['correoReceptor'],content['asunto'],content['cuerpo'])
-	return jsonify({'msg': x})
+	return jsonify({'msg': 'ok'})
 
 if __name__ == '__main__':
 	app.run(debug=False,host='0.0.0.0',port=8080)
