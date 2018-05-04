09c2fb5500675ed905f615489bc482583f6e4bbd
mdr.leon10 <mdr.leon10@ISIS2503S2-0011.sis.virtual.uniandes.edu.co>
2018-05-04 07:38:57 -0500

    Last changes

--- SistemaSeguridad-master/src/main/java/co/edu/uniandes/isis2503/sistemaseguridad/auth/AuthorizationFilter.java
+++ SistemaSeguridad-master/src/main/java/co/edu/uniandes/isis2503/sistemaseguridad/auth/AuthorizationFilter.java
@@ -55,7 +55,8 @@ public class AuthorizationFilter implements ContainerRequestFilter {
         admin,
         yale,
         prop,
-        seguridad
+        seguridad,
+        notAvailable
     }
 
     @Context
