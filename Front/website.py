976731ff0e8c46e3711d39c0bd892661a41e33e9
MARIA DEL ROSARIO LEON CURE <mdr.leon10@sis.virtual.uniandes.edu.co>
2018-05-18 15:22:28 -0500

    working with unidades after login

--- Front/website.py
+++ Front/website.py
@@ -1,11 +1,3 @@
-#Importados de websockets
-import json
-import threading
-from threading import Lock
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-from kafka import KafkaConsumer
-
 #Importados de auth0
 from functools import wraps
 import json
@@ -22,22 +14,20 @@ from flask import url_for
 from authlib.flask.client import OAuth
 from six.moves.urllib.parse import urlencode
 import requests
-
-
-
 from flask import Flask, jsonify, request
 
-#"threading", "eventlet" or "gevent", eventlet is the best
-#async_mode = eventlet 
 
+#Constantes
+path = "http://172.24.42.30:8080/api/"
+#Fin constantes
+
+#Declaracion APP
 app = Flask(__name__)
 app.secret_key = 'KgCeooHTX37S5LvbD-_FKhyFSr8GuzIcC0NANtB1sJtl8sMv3JcQP6ypCJtvezFH'
-# socketio = SocketIO(app, async_mode=async_mode)
-# thread = None
-# thread_lock = Lock()
-
 oauth = OAuth(app)
 
+
+#Declaracion AUTH0
 auth0 = oauth.register(
     'auth0',
     client_id='Y_BZT-Tyb4Z09mgbRkkfZrR7GWN1wJ4y',
@@ -54,39 +44,11 @@ auth0 = oauth.register(
 def index():
 	return render_template('index.html')
     
-
-# def background_thread_websocket():
-    # consumer = KafkaConsumer('TOPICO DE MOSQUITTOOO', group_id='temperature', bootstrap_servers=['DONDE ESTA KAFKA? EJ:localhost:8090'])
-    # for message in consumer:
-        # json_data = json.loads(message.value.decode('utf-8'))
-        # topicmqtt = json_data['topic']
-        # messagemqtt = json_data['message']
-        # residencia = topicmqtt.split('/')[1]
-        
-        # payload = {
-            # 'residencia': residencia,
-            # 'message': messagemqtt
-        # }
-        # socketio.emit('alarms', str(payload),
-                      # namespace='/yale')
-                      
-              
-#Cada vez que se conecta un cliente              
-# @socketio.on('connect', namespace='/thermalcomfort')
-# def test_connect():
-    # global thread
-    # with thread_lock:
-        # if thread is None:
-            # thread = socketio.start_background_task(target=background_thread_websocket)
-    # emit('alarms', "Connected!!!")                      
-                      
-    
-# Here we're using the /callback route.
 @app.route('/callback')
 def callback_handling():
     # Handles response from token endpoint
     resp = auth0.authorize_access_token()
-
+    id_token = resp['id_token']
     url = 'https://isis2503-jagomez1.auth0.com/userinfo'
     headers = {'authorization': 'Bearer ' + resp['access_token']}
     resp = requests.get(url, headers=headers)
@@ -94,15 +56,15 @@ def callback_handling():
 
     # Store the tue user information in flask session.
     session['jwt_payload'] = userinfo
-
+    
+    session['id_token'] = id_token 
+    
     session['profile'] = {
         'user_id': userinfo['sub'],
         'name': userinfo['name'],
         'picture': userinfo['picture']
     }
-
     return redirect('/unidades')
-    
 @app.route('/dashboard')
 def dashboard():
     print(session['profile'])
@@ -114,15 +76,17 @@ def dashboard():
                            
 @app.route('/unidades')
 def unidades():
+    headers = {'Authorization': 'Bearer ' + session['id_token']}
+    data = requests.get(path + 'unidadresidencial/', headers = headers).json()
+
     return render_template('unidades.html',
-                           userinfo=session['profile'],
-                           userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
+                           unidades = data)
 
 
     
 @app.route('/login')
 def login():
-    return auth0.authorize_redirect(redirect_uri='http://localhost:9160/callback', audience='https://isis2503-jagomez1.auth0.com/userinfo')
+    return auth0.authorize_redirect(redirect_uri='http://localhost:9080/callback', audience='https://isis2503-jagomez1.auth0.com/userinfo')
 
 @app.route('/logout')
 def logout():
@@ -131,7 +95,7 @@ def logout():
     return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
     
 if __name__ == '__main__':
-	app.run(debug=False,host='0.0.0.0',port=9160)
+	app.run(debug=False,host='0.0.0.0',port=9080)
     
     
     
