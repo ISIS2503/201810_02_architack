8e489997a1c67ed11f44f1168ccc971b73e8c807
JUAN CAMILO RUIZ PUENTES <jc.ruiz@sis.virtual.uniandes.edu.co>
2018-05-19 02:05:37 -0500

    Working colors

--- Front/static/functions/DetalleInmueble.js
+++ Front/static/functions/DetalleInmueble.js
@@ -2,7 +2,7 @@ function analizarSilenciar(id, silenciada) {
     console.log(silenciada);
     if(silenciada == "False") {
         var boton = document.getElementById(id+"Boton");
-        boton.class = "btn btn-danger";
+        boton.className = "btn btn-danger";
         boton.innerHTML = "Silenciar";
         boton.disabled = false;
     }
